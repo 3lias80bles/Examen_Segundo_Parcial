346599c2f82d4af2ed38f0e88cd5a4cdbc97c71f
3lias80bles <roblessergioelias@gmail.com>
2025-05-09 14:05:20 -0600

    Se sube nuevo archivo del examen >>Ocurrieron conflictos al subir carpetas

---
+++ examen_segundo_parcial/lib/pantallas/pantalla_principal.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:examen_segundo_parcial/pantallas/seleccionar_semestre.dart';
+
+class PantallaPrincipal extends StatelessWidget {
+  const PantallaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.people_alt_outlined,
+              size: 200,
+              color: Colors.blueGrey[600],
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'Sistema de Sorteos',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 0, 0, 0),
+                letterSpacing: 1.2,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                'Selecciona alumnos ganadores de manera aleatoria',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.blueGrey[600],
+                  height: 1.4,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            OutlinedButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SeleccionSemestre(),
+                ),
+              ),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.blue[600],
+                side: BorderSide(color: Colors.blue[400]!),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 36,
+                  vertical: 16,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              //Se agrego un Row para los iconos
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.play_arrow_sharp, size: 20),
+                  SizedBox(width: 8),
+                  Text(
+                    'Iniciar Sorteo',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
